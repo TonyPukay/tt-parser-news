70d3869bd49ea7dc8696ac09dfadbaa7e45d0067
tony <anton96pukay@gmail.com>
2018-02-08 01:33:15 +0200

    Create router for start page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ParsingController@viewWelcome');
+
+Route::get('/parsing', 'ParsingController@viewParsing');
+
+Route::get('/history', 'ParsingController@viewHistory');
